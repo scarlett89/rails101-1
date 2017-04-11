7c94ed6ed9ee5fe8ee3f47cce0608eb19b1bd6d1
tangjia <scarletttang89@163.com>
2017-04-12 06:20:30 +0800

    user can login/logout/signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= reqiure bootstrap/alerttouch
+//= reqiure bootstrap/alert
+//= require bootstrap/dropdown
 //= require_tree .
