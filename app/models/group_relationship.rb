cb99acdcea48e66e6dc5dfc6cb24ca1a3a1981f7
tangjia <scarletttang89@163.com>
2017-04-15 08:06:26 +0800

    implement group relation

--- app/models/group_relationship.rb
+++ app/models/group_relationship.rb
@@ -1,7 +1,4 @@
 class GroupRelationship < ApplicationRecord
   belongs_to :group
   belongs_to :user
-
-  has_many :group_relationships
-  has_many :members, through: :group_relationships, source: :user
 end
