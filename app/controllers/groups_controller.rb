6c90dbeef9a3907696c96816eb3f90ca275d1d00
tangjia <scarletttang89@163.com>
2017-04-17 14:57:04 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+
+    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -12,7 +13,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    find_group_and_check_permission
   end
 
   def new
@@ -30,11 +30,7 @@ class GroupsController < ApplicationController
     end
   end
 
-
   def update
-
-    find_group_and_check_permission
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -43,10 +39,36 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    find_group_and_check_permission
 
     @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
+  def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版本的成员了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版! "
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
